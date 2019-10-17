602ae5f73954bcb5d895c3262498e9cbb00067c0
CHOIMINSEOK <cms3718@gmail.com>
2019-10-17 23:15:59 +0900

    Sample BottomTabNavigation 구현

--- App.tsx
+++ App.tsx
@@ -1,49 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
 import React from 'react';
-import {Text} from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {Text, View} from 'react-native';
+import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {ContactlistScreen} from './view/ContactListScreen';
 import {SettingsScreen} from './view/SettingsScreen';
 
-// RouteConfig로 분리할 수 있을 것 같다.
-const MainTabs = createBottomTabNavigator(
-  {
-    Contacts: ContactlistScreen,
-    Settings: SettingsScreen,
-  },
-  {
-    tabBarOptions: {
-      activeTintColor: '#a41034',
-    },
-    defaultNavigationOptions: ({navigation}) => ({
-      tabBarIcon: ({tintColor}) => {
-        const {routeName} = navigation.state;
-        let iconName;
-        if (routeName === 'Contacts') {
-          iconName = 'ios-contacts';
-        } else if (routeName === 'Settings') {
-          iconName = 'ios-options';
-        }
+const TabNavigator = createBottomTabNavigator({
+  ContactList: ContactlistScreen,
+  Settings: SettingsScreen,
+});
 
-        // You can return any component that you like here!
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-  },
-);
-
-const App = () => {
-  return MainTabs;
-};
-
-export default App;
+export default createAppContainer(TabNavigator);
--- view/SettingsScreen.tsx
+++ view/SettingsScreen.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import {Text, View} from 'react-native';
 
 export const SettingsScreen = () => {
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>SettingsScreen!</Text>
     </View>
   );
 };
