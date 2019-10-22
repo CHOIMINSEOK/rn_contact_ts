30058811c8643a880d112dd9c9c926877ce73de1
CHOIMINSEOK <cms3718@gmail.com>
2019-10-22 21:37:04 +0900

    Request Body 핸들링하는 로직을 서비스로 옮김

--- src/data/UserRepository.ts
+++ src/data/UserRepository.ts
@@ -6,12 +6,6 @@ import User from '../domain/User';
 export default class UserRepository implements EmployeeRepositoryInterface {
   public async getUsers(): Promise<Array<User>> {
     // 상세 구현 필요
-    const request = fetchUsers();
-    return request
-      .then(response => response.json())
-      .then(body => {
-        console.warn(body);
-        return body.results.map(user => new User(user));
-      });
+    return fetchUsers();
   }
 }
--- src/data/remote/UserService.ts
+++ src/data/remote/UserService.ts
@@ -1,5 +1,9 @@
 import User from '../../domain/User';
 
-export const fetchUsers = async (): Promise<Response> => {
-  return await fetch('https://randomuser.me/api/?results=50&nat=us');
+export const fetchUsers = async (): Promise<Array<User>> => {
+  return fetch('https://randomuser.me/api/?results=50&nat=us')
+    .then(response => response.json())
+    .then(body => {
+      return body.results.map(user => new User(user));
+    });
 };
